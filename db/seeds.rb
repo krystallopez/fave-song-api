e8d7520b25e37b95369db932f0c1130fd8e73625
Krystal Lopez <lopezkrystal10@gmail.com>
2022-06-30 17:16:38 -0700

    added seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,19 @@
 Song.create({title: "Honey", album: "Butterfly", artist: "Mariah Carey", year: 1997})
+
+Song.create({title: "Che Che Cole", album: "Crime Pays", artist: "Willie Colon and Hector Lavoe", year: 1972})
+
+Song.create({title: "The Only One", album: "The Open Door", artist: "Evanescence", year: 2006})
+
+Song.create ({title: "I'm So Sick", album: "Flyleaf", artist: "Flyleaf", year: 2006})
+
+Song.create({title: "Trapped in a Box", album: "No Doubt", artist: "No Doubt", year: 1992})
+
+Song.create({title: "Hold Tight", album: "Dave Dee, Dozy, Beaky, Mich, & Tich", artist:  "Dave Dee, Dozy, Beaky, Mich, & Tich", year: 1966})
+
+Song.create({title: "Bilingue", album: "Bilingue", artist: "Snow Tha Product", year: 2019})
+
+Song.create({title: "Si Una Vez", album: "Amor Prohibido", artist: "Selena", year: 1994})
+
+Song.create({title: "Wait a Minute!", album: "ARDIPITHECUS", artist: "WILLOW", year: 2015})
+
+puts "Done."
